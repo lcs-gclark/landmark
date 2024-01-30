fe7eea257d20a72944f197df1f45ccf6a2798f9d
grayden clark <gclark@lcs.on.ca>
2024-01-30 14:57:16 -0500

    lots done good commit

---
+++ landmark/Model/Landmark.swift
@@ -0,0 +1,18 @@
+//
+//  Landmark.swift
+//  landmark
+//
+//  Created by Gray Clark on 2024-01-30.
+//
+
+import SwiftUI
+
+struct Landmark: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Landmark()
+}
--- landmark/landmarkApp.swift
+++ landmark/landmarkApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct landmarkApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DetailView()
         }
     }
 }

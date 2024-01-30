126935b135fa13a68cf0d25e3982987b5df1dbf3
grayden clark <gclark@lcs.on.ca>
2024-01-30 14:34:02 -0500

    Initial Commit

---
+++ landmark/landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkApp.swift
+//  landmark
+//
+//  Created by Gray Clark on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

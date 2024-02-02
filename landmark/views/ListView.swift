d35e9b550d129d3be940488b145fab5d164663bd
grayden clark <gclark@lcs.on.ca>
2024-02-02 09:10:18 -0500

    lots done list close to finidhd

---
+++ landmark/views/ListView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        
+        NavigationStack {
+            
+            List(allLandmarks) { currentLandmark in
+                
+                NavigationLink {
+                    DetailView(item: currentLandmark)
+                } label: {
+                    Text(currentLandmark.name)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+            
+                        
+        }
+        
+    }
+}
+
+#Preview {
+    ListView()
+}
